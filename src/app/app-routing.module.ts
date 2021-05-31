ab98e82442fcab5dd3203dca725d873efa0eba5d
Eimi Pinzon <eimipinzonhoyos@gmail.com>
2021-05-31 12:25:45 -0500

    Se crean más páginas y las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemsComponent } from './pages/items/items.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PortafolioComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'items', component: ItemsComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { };
