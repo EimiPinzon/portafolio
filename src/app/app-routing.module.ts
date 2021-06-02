585505de3e203295f7a5e5712472248f9262a137
Eimi Pinzon <eimipinzonhoyos@gmail.com>
2021-06-02 12:03:23 -0500

    funciona el menú y el footer, se habilitan las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,14 @@ import { AboutComponent } from './pages/about/about.component';
 import { ItemsComponent } from './pages/items/items.component';
 
 const routes: Routes = [
-  {path: '', component: PortafolioComponent},
+  {path: 'home', component: PortafolioComponent},
   {path: 'about', component: AboutComponent},
   {path: 'items', component: ItemsComponent},
-  
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { };
